3bdee8be54f4ad8d1970e63d004e1899a1b4d277
Joaquin <joal.fernandez@alumno.um.edu.ar>
2025-06-09 20:10:18 -0300

    agrego test exceptions

--- test/test_especialidad.py
+++ test/test_especialidad.py
@@ -2,8 +2,12 @@ import unittest
 from modelo.especialidad import Especialidad
 from modelo.exceptions import EspecialidadNoEncontrada
 
-class TestEspecialidad(unittest.TestCase):
-    def especialidad_disponible(self):
+class testEspecialidad(unittest.TestCase):
+    def test_especialidad_disponible(self):
         e = Especialidad("Pediatra", ["lunes", "martes", "viernes"])
         self.assertEqual(e.tipo, "Pediatra")
         self.assertEqual(e.dia, ["lunes", "martes", "viernes"])
+
+
+if __name__ == '__main__':
+    unittest.main()
