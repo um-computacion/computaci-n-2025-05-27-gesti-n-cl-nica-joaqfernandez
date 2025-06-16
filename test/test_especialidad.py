bd642159269fe558e7c635e72068be72267edfac
Joaquin <joal.fernandez@alumno.um.edu.ar>
2025-06-16 13:09:12 -0300

    modifico el nombre del atributo de test_especialidad

--- test/test_especialidad.py
+++ test/test_especialidad.py
@@ -5,7 +5,7 @@ from modelo.exceptions import EspecialidadNoEncontrada
 class testEspecialidad(unittest.TestCase):
     def test_especialidad_disponible(self):
         e = Especialidad("Pediatra", ["lunes", "martes", "viernes"])
-        self.assertEqual(e.tipo, "Pediatra")
+        self.assertEqual(e.nombre, "Pediatra")
         self.assertEqual(e.dia, ["lunes", "martes", "viernes"])
 
 
