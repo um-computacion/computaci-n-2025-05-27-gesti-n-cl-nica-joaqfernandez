e623b85ede08c902f13330cb59b8adac025e6398
Joaquin <joal.fernandez@alumno.um.edu.ar>
2025-06-16 13:10:21 -0300

    agrego la especialidad en el test

--- test/test_medico.py
+++ test/test_medico.py
@@ -1,13 +1,17 @@
 import unittest
 from modelo.medico import Medico
+from modelo.especialidad import Especialidad
 
 class testMedico(unittest.TestCase):
     def test_medico(self):
-        med = Medico("Marcela", "Leuzzi", 56643128, 2145 )
+        esp = Especialidad("Pediatra", ["Lunes", "Martes"])
+        med = Medico("Marcela", "Leuzzi", 56643128, 2145, [esp])
         self.assertEqual(med.nombre, "Marcela")
         self.assertEqual(med.apellido, "Leuzzi")
         self.assertEqual(med.matricula, 2145)
         self.assertEqual(med.dni, 56643128)
+        self.assertEqual(med.especialidades[0].nombre, "Pediatra")
+
 
 if __name__ == '__main__':
     unittest.main()
