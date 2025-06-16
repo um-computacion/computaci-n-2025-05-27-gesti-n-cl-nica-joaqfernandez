2a63bd88c3b3f9ea853ff05270e597ca16a51070
Joaquin <joal.fernandez@alumno.um.edu.ar>
2025-06-16 13:08:10 -0300

    agrego atributo especialidad a medico

--- modelo/medico.py
+++ modelo/medico.py
@@ -1,9 +1,13 @@
-class Medico():
-    def __init__(self, nombre, apellido, dni, matricula):
+from modelo.especialidad import Especialidad
+
+class Medico:
+    def __init__(self, nombre, apellido, dni, matricula, especialidades):
         self.nombre = nombre
         self.apellido = apellido
         self.dni = dni
         self.matricula = matricula
+        self.especialidades = especialidades
+
 
     def __str__(self):
         return f"{self.nombre} {self.apellido}, MATRICULA: {self.matricula} DNI: {self.dni}"
