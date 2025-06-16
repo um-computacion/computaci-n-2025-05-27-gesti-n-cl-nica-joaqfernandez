e83f87f77f98a04ca1f51faf27e0afbc1bb2874d
Joaquin <joal.fernandez@alumno.um.edu.ar>
2025-06-16 13:05:58 -0300

    corrigo el archivo modelo

--- modelo/turno.py
+++ modelo/turno.py
@@ -1,7 +1,31 @@
-from modelo.medico import Medico
-from modelo.paciente import Paciente
-from modelo.especialidad import Especialidad
+from modelo.exceptions import EspecialidadNoEncontrada, TurnoOcupado, DiaNoDisponible
 
 
-class Turnos():
+class Turnos:
+    turnos_ocupados = []
+
+    def __init__(self, paciente, especialidad, medico, dia, hora):
+        self.paciente = paciente
+        self.especialidad = especialidad
+        self.medico = medico
+        self.dia = dia
+        self.hora = hora
     
+        especialidad_encontrada = False
+    
+        for e in medico.especialidades:
+            # Verificar si atiende ese día
+            try:
+                e.verificar_disponible(dia)
+                especialidad_encontrada = True
+                break
+            except DiaNoDisponible:
+                continue
+        
+        if not especialidad_encontrada:
+            raise EspecialidadNoEncontrada(f"El medico no atiende los dias {dia}")
+        
+        if (dia, hora) in Turnos.turnos_ocupados:
+            raise TurnoOcupado(f"Turno ocupado el {dia}, a las {hora}")
+        
+        Turnos.turnos_ocupados.append((dia, hora))
